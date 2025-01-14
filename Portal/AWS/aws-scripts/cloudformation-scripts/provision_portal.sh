57ff3e532d4883feecebc46b6f73c043eb121694
AkashSharmaInfrablok <asharma@infrablok.com>
2025-01-14 12:14:44 +0530

    updated scripts for portal to add ecr cross account support

--- Portal/AWS/aws-scripts/cloudformation-scripts/provision_portal.sh
+++ Portal/AWS/aws-scripts/cloudformation-scripts/provision_portal.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-source user-config.sh
+source user-config-1.sh
+
+# Define the target file (e.g., your CloudFormation template or any script)
+target_file="portal_cf.yaml"
+
+# Perform replacements using sed
+# Update Cross Account Setup for ECR access
+sed -i "s|ECR_ROLE_ARN=\".*\"|ECR_ROLE_ARN=\"${ECR_ROLE_ARN}\"|" "$target_file"
+sed -i "s|ECR_ROLE_SESSION_NAME=\".*\"|ECR_ROLE_SESSION_NAME=\"${ECR_ROLE_SESSION_NAME}\"|" "$target_file"
+sed -i "s|ECR_ROLE_EXTERNAL_ID=\".*\"|ECR_ROLE_EXTERNAL_ID=\"${ECR_ROLE_EXTERNAL_ID}\"|" "$target_file"
+
+# Update Secret Manager values
+sed -i "s|SECRET_BASE_NAME: \".*\"|SECRET_BASE_NAME: \"${SECRET_BASE_NAME}\"|" "$target_file"
+sed -i "s|SECRET_REGION_NAME: \".*\"|SECRET_REGION_NAME: \"${SECRET_REGION_NAME}\"|" "$target_file"
+
+# Update Cognito values
+sed -i "s|COGNITO_CLIENT_ID: \".*\"|COGNITO_CLIENT_ID: \"${COGNITO_CLIENT_ID}\"|" "$target_file"
+sed -i "s|COGNITO_USER_POOL_ID: \".*\"|COGNITO_USER_POOL_ID: \"${COGNITO_USER_POOL_ID}\"|" "$target_file"
+sed -i "s|COGNITO_REGION: \".*\"|COGNITO_REGION: \"${COGNITO_REGION}\"|" "$target_file"
+sed -i "s|NEW_USERNAME: \".*\"|NEW_USERNAME: \"${NEW_USERNAME}\"|" "$target_file"
+
+# Print a message indicating completion
+echo "Replacements completed successfully in $target_file."
 
 # Creating Portal Instance
 # Create stack
--- Portal/AWS/aws-scripts/cloudformation-scripts/user-config.sh
+++ Portal/AWS/aws-scripts/cloudformation-scripts/user-config.sh
@@ -10,6 +10,21 @@ PORTAL_REGION="us-east-1"
 SSHKeyname="portal"
 InstanceType="t2.xlarge"
 
+#Cognito Parameters
+COGNITO_CLIENT_ID="pass-cognito-client-id"          # Update Cognito Client ID
+COGNITO_USER_POOL_ID="pass-cognito-user-pool-id"    # Update Cognito User Pool ID
+COGNITO_REGION="us-east-1"                          # Update Cognito Region
+NEW_USERNAME="user@domain.com"                        # Provide First User name
+
+#Secret Manager Parameters.
+SECRET_BASE_NAME="secret_name"                   # Secret Name
+SECRET_REGION_NAME="us-east-1"                             # AWS Region for Secrets Manager
+
+# Cross-Account ECR Access Setup
+ECR_ROLE_ARN="arn:aws:iam::637423168201:role/edna-ecr-role"     # Replace <SOURCE_ACCOUNT_ID> and <ROLE_NAME>
+ECR_ROLE_SESSION_NAME="ECRAssumeRoleSession"                    # Replace session_name with any desired value (temporary)
+ECR_ROLE_EXTERNAL_ID="external-id-12345"                        # Replace <ECR_ROLE_EXTERNAL_ID> with the provider's external ID
+
 # ===============================
 # Leave These Fields Unchanged (Do Not Edit)
 # ===============================
