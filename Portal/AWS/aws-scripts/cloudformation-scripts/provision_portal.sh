77117187e120ff08ad1c4458f142f67e955c5915
AkashSharmaInfrablok <asharma@infrablok.com>
2025-01-17 15:11:48 +0530

    Updated portal scripts

--- Portal/AWS/aws-scripts/cloudformation-scripts/provision_portal.sh
+++ Portal/AWS/aws-scripts/cloudformation-scripts/provision_portal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source user-config-1.sh
+source user-config.sh
 
 # Define the target file (e.g., your CloudFormation template or any script)
 target_file="portal_cf.yaml"
@@ -11,7 +11,7 @@ sed -i "s|ECR_ROLE_SESSION_NAME=\".*\"|ECR_ROLE_SESSION_NAME=\"${ECR_ROLE_SESSIO
 sed -i "s|ECR_ROLE_EXTERNAL_ID=\".*\"|ECR_ROLE_EXTERNAL_ID=\"${ECR_ROLE_EXTERNAL_ID}\"|" "$target_file"
 
 # Update Secret Manager values
-sed -i "s|SECRET_BASE_NAME: \".*\"|SECRET_BASE_NAME: \"${SECRET_BASE_NAME}\"|" "$target_file"
+sed -i "s|SECRET_NAME: \".*\"|SECRET_NAME: \"${SECRET_NAME}\"|" "$target_file"
 sed -i "s|SECRET_REGION_NAME: \".*\"|SECRET_REGION_NAME: \"${SECRET_REGION_NAME}\"|" "$target_file"
 
 # Update Cognito values
@@ -26,7 +26,7 @@ echo "Replacements completed successfully in $target_file."
 # Creating Portal Instance
 # Create stack
 echo "Creating stack $PORTAL_STACK_NAME in AWS CloudFormation..."
-create_output=$(aws cloudformation create-stack --stack-name $PORTAL_STACK_NAME --template-body $PORTAL_TEMPLATE_BODY --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameters ParameterKey=SSHKeyname,ParameterValue=$SSHKeyname ParameterKey=InstanceType,ParameterValue=$InstanceType --region $PORTAL_REGION)
+create_output=$(aws cloudformation create-stack --stack-name $PORTAL_STACK_NAME --template-body $PORTAL_TEMPLATE_BODY --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameters ParameterKey=SSHKeyname,ParameterValue=$SSHKeyname ParameterKey=InstanceType,ParameterValue=$InstanceType ParameterKey=ExistingIAMRoleName,ParameterValue=$ExistingIAMRoleName --region $PORTAL_REGION)
 
 # Wait for the stack to be created
 echo "Waiting for stack to be created..."
@@ -35,6 +35,13 @@ aws cloudformation wait stack-create-complete --stack-name $PORTAL_STACK_NAME --
 # Check if stack creation was successful
 if [ $? -eq 0 ]; then
     echo "Stack $PORTAL_STACK_NAME created successfully."
+    
+    # Retrieve the stack outputs and extract the ALB DNS Name
+    ALBDNSName=$(aws cloudformation describe-stacks --stack-name $PORTAL_STACK_NAME --region $PORTAL_REGION \
+        --query "Stacks[0].Outputs[?OutputKey=='ALBDNSName'].OutputValue" --output text)
+    
+    # Display the ALB DNS Name
+    echo "ALB DNS Name: $ALBDNSName"
 else
     echo "Failed to create stack $PORTAL_STACK_NAME."
     exit 1
--- Portal/AWS/aws-scripts/cloudformation-scripts/user-config.sh
+++ Portal/AWS/aws-scripts/cloudformation-scripts/user-config.sh
@@ -9,15 +9,16 @@ PORTAL_STACK_NAME="Portal-ec2-stack"
 PORTAL_REGION="us-east-1"
 SSHKeyname="portal"
 InstanceType="t2.xlarge"
+ExistingIAMRoleName="Portal_ECR_Role"               # Provide the Role name created for ECR Cross Account Setup
 
 #Cognito Parameters
 COGNITO_CLIENT_ID="pass-cognito-client-id"          # Update Cognito Client ID
 COGNITO_USER_POOL_ID="pass-cognito-user-pool-id"    # Update Cognito User Pool ID
 COGNITO_REGION="us-east-1"                          # Update Cognito Region
-COGNITO_USER_MAIL_ID="user@domain.com"                        # Provide First User name
+COGNITO_USER_MAIL_ID="user@domain.com"              # Provide Mail ID of the Cognito User
 
 #Secret Manager Parameters.
-SECRET_BASE_NAME="secret_name"                   # Secret Name
+SECRET_NAME="secret_name"                   # Secret Name
 SECRET_REGION_NAME="us-east-1"                             # AWS Region for Secrets Manager
 
 # Cross-Account ECR Access Setup
